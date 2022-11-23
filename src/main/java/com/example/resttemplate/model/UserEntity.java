ef883a3fe95c3eaeef4a875ea9ada0f29fcd9521
David Ho <davidho0403@gmail.com>
2022-11-24 01:54:22 +0800

    init constructor, getter, setter

--- src/main/java/com/example/resttemplate/model/UserEntity.java
+++ src/main/java/com/example/resttemplate/model/UserEntity.java
@@ -12,12 +12,35 @@ public class UserEntity {
     private String name;
     private String email;
 
-    @Override
-    public String toString() {
-        return "UserEntity{" +
-                "userID=" + userID +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                '}';
+    public UserEntity() {}
+
+    public UserEntity(Integer userID, String name, String email) {
+        this.userID = userID;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Integer getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Integer userID) {
+        this.userID = userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
