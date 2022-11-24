c159c9f22078a2d54c7a76772f3e1aed25747d72
David Ho <davidho0403@gmail.com>
2022-11-25 01:13:34 +0800

    import all package in we.bind

--- src/main/java/com/example/resttemplate/Controller.java
+++ src/main/java/com/example/resttemplate/Controller.java
@@ -3,9 +3,7 @@ package com.example.resttemplate;
 import com.example.resttemplate.model.UserEntity;
 import com.example.resttemplate.model.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
